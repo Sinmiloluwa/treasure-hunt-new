c531a4453b60b8647876435e549c5e5b39ec0e7f
Sinmiloluwa <mofeoluwae@gmail.com>
2025-12-01 11:54:47 +0100

    updated screen

--- lib/screens/team_view_screen.dart
+++ lib/screens/team_view_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:treasure_hunt/constants/text_styles.dart';
 import 'package:treasure_hunt/screens/mission_screen.dart';
+import 'package:treasure_hunt/widgets/build_hunt_card.dart';
 
 class TeamViewScreen extends StatelessWidget {
   const TeamViewScreen({super.key});
@@ -67,7 +68,7 @@ class TeamViewScreen extends StatelessWidget {
                       childAspectRatio: 0.75),
                   itemBuilder: (context, index) {
                     final imageUrl = teamImages[index];
-                    return _buildCard(imageUrl, context);
+                    return HuntCardWidget(imageUrl: imageUrl);
                   },
                 )
               ]),
@@ -88,103 +89,6 @@ class TeamViewScreen extends StatelessWidget {
         icon,
         color: Color(0xFF933DFC),
         size: 26,
-      ),
-    );
-  }
-
-  Widget _buildCard(String imageUrl, BuildContext context) {
-    return Container(
-      height: 300,
-      decoration: BoxDecoration(
-        color: Color(0xFF1A1A1A),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
-            child: CachedNetworkImage(
-              imageUrl: imageUrl,
-              height: 100,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(height: 10),
-          const Center(
-            child: Text(
-              'Team Name',
-              style: AppTextStyles.body,
-            ),
-          ),
-          const SizedBox(height: 5),
-          const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Icon(
-              Icons.people,
-              size: 16,
-              color: Colors.white70,
-            ),
-            SizedBox(width: 4),
-            Text(
-              '5 Members',
-              style: AppTextStyles.body,
-            ),
-          ]),
-          const SizedBox(height: 5),
-          const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Icon(
-              Icons.location_on,
-              size: 16,
-              color: Colors.white70,
-            ),
-            SizedBox(width: 4),
-            Text(
-              'Melbourne, Australia',
-              style: AppTextStyles.body,
-            ),
-          ]),
-          const SizedBox(height: 5),
-          ElevatedButton(
-            onPressed: (){
-              showDialog(
-              context: context,
-              builder: (dialogContext) => AlertDialog(
-                title: const Text('Terms & Conditions'),
-                content: const Text(
-                  'Please accept the terms and conditions to continue.'
-                ),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(), 
-                    child: const Text('Cancel', style: AppTextStyles.heading),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (_) => MissionScreen(imageUrl: imageUrl),
-                        ),
-                      );
-                    },
-                    child: Text('Accept', style: AppTextStyles.heading),
-                  ),
-                ],
-              ),
-            );
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFF933DFC),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-            ), 
-            child: Text('Join Team', style: AppTextStyles.heading.copyWith(fontSize: 16)),
-            )
-        ],
       ),
     );
   }
