d84a7eda7468aa25c56e2922ab361429aa39ea3b
Sinmiloluwa <mofeoluwae@gmail.com>
2025-12-01 11:54:34 +0100

    single view screen refactor

--- lib/screens/single_view_screen.dart
+++ lib/screens/single_view_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:treasure_hunt/constants/text_styles.dart';
+import 'package:treasure_hunt/widgets/card_widget.dart';
+import 'package:treasure_hunt/widgets/filter_button.dart';
 import 'package:treasure_hunt/widgets/search_bar_widget.dart';
 
 class SingleViewScreen extends StatelessWidget {
@@ -24,11 +26,47 @@ class SingleViewScreen extends StatelessWidget {
                 children: [
                   SearchField(),
                   SizedBox(height: 20),
-                  Text(
-                    'Here you can enjoy the treasure hunt on your own. Explore the clues, solve puzzles, and find the treasure at your own pace. Good luck and have fun!',
-                    style: TextStyle(fontSize: 16),
-                    textAlign: TextAlign.center,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Results'),
+                              Text('Showing 2 results for Lagos, Nigeria',
+                                  style: AppTextStyles.cardSubtitle
+                                      .copyWith(color: Colors.grey)),
+                            ]),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: FilterButton(),
+                      )
+                    ],
                   ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 6,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 12,
+                      mainAxisSpacing: 30,
+                      childAspectRatio: 0.9,
+                    ),
+                    itemBuilder: (context, index) {
+                      return CardWidget(
+                        imageUrl:
+                            'https://plus.unsplash.com/premium_photo-1682390303366-7463dcbec281?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                            type: 'single',
+                      );
+                    },
+                  )
                 ],
               ),
             ),
